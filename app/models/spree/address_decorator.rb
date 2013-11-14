8a8cd4e5fda94a9b063fc51e02c6fdeab31c6f4c
laurobecker <lauro.becker@gmail.com>
2013-11-14 10:38:14 -0200

    Integração com campos do PagSeguro

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -2,7 +2,7 @@ Spree::Address.class_eval do
     
     usar_como_cpf :cpf
     
-    attr_accessible :cpf, :phone_ddd, :address_number
+    # attr_accessible :cpf, :phone_ddd, :address_number
     
     validates :cpf, :address_number, :presence => true
     validates :phone_ddd, :presence => true, :if => :require_phone?
