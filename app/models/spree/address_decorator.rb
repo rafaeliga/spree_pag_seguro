6a8e4b9c096587366e2b88b665c66e315aa7419d
Leonardo S. Meira <leosmeira@gmail.com>
2013-11-18 14:58:25 -0200

    Modificações para Spree 1.2.2

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -5,7 +5,6 @@ Spree::Address.class_eval do
     attr_accessible :cpf, :phone_ddd, :address_number
     
     validates :cpf, :address_number, :presence => true
-    validates :phone_ddd, :presence => true, :if => :require_phone?
     
     def full_phone
       "#{phone_ddd} #{phone}".strip
