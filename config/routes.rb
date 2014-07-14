46c2bbf07fb62387ede4f95e6aed8232b7cbf2ba
Leonardo S. Meira <desenvolvimento@agenciaprimore.com.br>
2014-07-14 19:18:11 -0300

    Atualizando as rotas para o Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Spree::Core::Engine.routes.prepend do
-  # match "pag_seguro/notify", :to => "pag_seguro#notify", method: :post
-  # match "pag_seguro/callback", :to => "pag_seguro#callback", method: :get
-  # resources :payment_notifications, :only => [:create]
+Spree::Core::Engine.routes.draw do
+  patch "pag_seguro/notify", :to => "pag_seguro#notify"
+  get "pag_seguro/callback", :to => "pag_seguro#callback"
+  resources :payment_notifications, :only => [:create]
 end
