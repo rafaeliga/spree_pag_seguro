ca63e7c54a54b1fa3f0508e7b117b4fb3a04a232
Leonardo S. Meira <desenvolvimento@agenciaprimore.com.br>
2014-07-15 14:33:06 -0300

    correção das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  patch "pag_seguro/notify", :to => "pag_seguro#notify"
-  get "pag_seguro/callback", :to => "pag_seguro#callback"
+  post "pag_seguro/notify", :to => "pag_seguro#notify"
+  get  "pag_seguro/callback", :to => "pag_seguro#callback"
   resources :payment_notifications, :only => [:create]
 end
