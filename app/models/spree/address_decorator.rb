5e4ddef4d144f3ed4427fc6b7648cb673708d67c
Leonardo S. Meira <desenvolvimento@agenciaprimore.com.br>
2014-07-23 15:56:35 -0300

    Remoção do campo "phone_ddd"

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -5,18 +5,9 @@ Spree::Address.class_eval do
     attr_accessor :require_phone, :require_cpf
 
     validates :cpf, :address_number, presence: true, if: :require_cpf?
-    validates :phone_ddd, presence: true, if: :require_phone?
-
-    def full_phone
-      "#{phone_ddd} #{phone}".strip
-    end
 
     def require_cpf?
       @require_cpf ||= false
-    end
-
-    def require_phone?
-      @require_phone ||= false
     end
 
 end
--- db/migrate/20131101193941_add_columns_to_spree_address.rb
+++ db/migrate/20131101193941_add_columns_to_spree_address.rb
@@ -2,6 +2,5 @@ class AddColumnsToSpreeAddress < ActiveRecord::Migration
   def change
     add_column :spree_addresses, :cpf, :string
     add_column :spree_addresses, :address_number, :string
-    add_column :spree_addresses, :phone_ddd, :string
   end
 end
