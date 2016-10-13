ae8df520714840e284c764591601a9ec82069b37
Leonardo S. Meira <desenvolvimento@agenciaprimore.com.br>
2016-10-13 14:14:14 -0300

    fix

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -9,7 +9,7 @@ Spree::Order.class_eval do
     Spree::PaymentMethod.where(type: "Spree::PaymentMethod::PagSeguroMethod")
   end  
     
-  def pag_seguro_payment_method
+  def self.pag_seguro_payment_method
     payment_method_id = self.payments.last.payment_method_id
     return Spree::PaymentMethod.where(type: "Spree::PaymentMethod::PagSeguroMethod", id: payment_method_id).first
   end
